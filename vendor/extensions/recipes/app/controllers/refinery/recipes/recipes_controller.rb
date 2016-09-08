22c16576b465b9c74a1becdb57b0424e0c2cd934
IvanFadieiev <ivanfadeev91@gmail.com>
2016-09-08 16:58:18 +0300

    feature/articles

--- vendor/extensions/recipes/app/controllers/refinery/recipes/recipes_controller.rb
+++ vendor/extensions/recipes/app/controllers/refinery/recipes/recipes_controller.rb
@@ -22,7 +22,7 @@ module Refinery
     protected
 
       def find_all_recipes
-        @recipes = Recipe.paginate(:page => params[:page], :per_page => 3).order('position ASC')
+        @recipes = Recipe.paginate(:page => params[:page], :per_page => 3).order('created_at DESC')
       end
 
       def find_page
