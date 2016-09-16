07af5ce01984eb41aceee20c5a66b13bc81b8d39
IvanFadieiev <ivanfadeev91@gmail.com>
2016-09-16 12:43:01 +0300

    secondary_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :find_header_links
+  private
+
+  def find_header_links
+  	@menu_items = Refinery::Page.where( show_in_menu: true )
+  end
 end
