2f094de5ba357ba289f6922946505dd26ca2a3f9
IvanFadieiev <ivanfadeev91@gmail.com>
2016-09-19 21:01:28 +0300

    add/search_model

--- config/initializers/refinery/search.rb
+++ config/initializers/refinery/search.rb
@@ -1,5 +1,5 @@
 Refinery::Search.configure do |config|
-  # config.enable_for = ["Refinery::Page"]
+  config.enable_for = ["Refinery::Page", "Refinery::Blogs::Blog", "Refinery::Recipes::Recipe"]
   # config.page_url = "/search"
   # config.results_per_page = 10
 end
--- vendor/extensions/blogs/app/models/refinery/blogs/blog.rb
+++ vendor/extensions/blogs/app/models/refinery/blogs/blog.rb
@@ -10,8 +10,7 @@ module Refinery
 
       # To enable admin searching, add acts_as_indexed on searchable fields, for example:
       #
-      #   acts_as_indexed :fields => [:title]
-
+      acts_as_indexed fields: [:title, :body]
     end
   end
 end
