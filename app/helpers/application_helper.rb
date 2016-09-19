5d9ca0003b82400ce88fc0483792b18457617122
IvanFadieiev <ivanfadeev91@gmail.com>
2016-09-19 11:12:26 +0300

    addings_for_blog

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
 		def banner_alt(banner)
 			banner.image.alt || "Banner"
 		end
+
+		def truncator(msg)
+      if msg.body.length > 150
+        msg.body.truncate_words(30, omission: " ").html_safe
+      else
+        msg.body.html_safe
+      end
+		end
 end
