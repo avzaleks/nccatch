8fe88058d0bf2e43cb22f0f21ec122cd17bf2b8b
Denis Dotsenko <denisdotsenko7@gmail.com>
2016-12-14 11:12:37 +0200

    change staging config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,7 @@ set :deploy_to, '/apps/nccatch_staging'
 set :default_shell, '/bin/bash --login'
 
 set :rails_env, 'staging'
-set :branch, 'staging'
+set :branch, 'master'
 
 set :unicorn_env, 'staging'
 set :unicorn_pid, "#{deploy_to}/shared/tmp/pids/unicorn.pid"
