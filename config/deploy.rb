442051797aec4fdca7f19d54542b72f9fc9d2334
Danchek <denisdotsenko7@gmail.com>
2016-12-15 18:47:11 +0200

    update deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,15 +13,17 @@ set :linked_files, %w{config/database.yml config/secrets.yml}
 
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets public/system}
 
-set (:unicorn_pid)  { "#{deploy_to}/shared/pids/unicorn.pid" }
+set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
 
 set :keep_releases, 5
+
+
 
 namespace :unicorn do
 	desc "Rake unicorn_stop"
 	task :stop_server do
 	  on 'adam@23.92.28.86' do
-      run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+      run "if [ -f #{:unicorn_pid} ] && [ -e /proc/$(cat #{:unicorn_pid}) ]; then kill -QUIT `cat #{:unicorn_pid}`; fi"
 	  end
 	end
 end
