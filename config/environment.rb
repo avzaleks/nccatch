331fe483aab7943ed27179ded5c0717d0d058ae5
Danchek <denisdotsenko7@gmail.com>
2016-12-19 14:09:42 +0200

    change date format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Set date to American format
+Date::DATE_FORMATS.merge!(:default => "%m/%d/%Y")
